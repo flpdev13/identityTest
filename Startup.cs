7e940742aad7dcf94d33db9463eb8795ecf5ce75
Felipe Oliveira Souza <felipe.souza@soulisto.com.br>
2019-06-07 16:37:50 -0300

    Configuração de acesso ao MySql

--- Startup.cs
+++ Startup.cs
@@ -34,11 +34,8 @@ namespace identityTest
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>()
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
